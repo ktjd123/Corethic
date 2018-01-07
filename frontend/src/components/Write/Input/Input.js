6e1d1ac8ba574d32c31bf2b653f039aae2763408
ktjd1262 <k9326239@gmail.com>
2018-01-07 17:39:01 +0900

    selection

--- frontend/src/components/Write/Input/Input.js
+++ frontend/src/components/Write/Input/Input.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './Input.css'
 
+class Type extends Component {
+    render() {
+        return (
+            <select>
+                <option value='호재'>호재</option>
+                <option value='악재'>악재</option>
+            </select>
+        )
+    }
+}
+
 class Input extends Component {
     render() {
         return (
             <div className='input'>
-                <input className='title' placeholder='제목을 입력해주세요'/>
-                <div className='count'>0/15</div>
+                <div className='inputBox'>
+                    <input className='title' placeholder='제목을 입력해주세요' />
+                    <div className='count'>0/15</div>
+                </div>
+                <Type/>
             </div>
         );
     }
