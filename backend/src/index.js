7ba13d549c3ab42e0e6a695e5279ac8871f439c4
ktjd1262 <k9326239@gmail.com>
2018-01-09 02:59:16 +0900

    robots.txt

--- backend/src/index.js
+++ backend/src/index.js
@@ -49,6 +49,9 @@ app.use('/api', api);
 
 //client side routing support
 app.use(express.static(path.resolve(__dirname, "..", "frontend")))
+app.get('/robots.txt', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "robots.txt"))
+})
 app.get('*', (req,res)=> {
     res.sendFile(path.resolve(__dirname, "..", "frontend", "index.html"))
 })
