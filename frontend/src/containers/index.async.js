6bcebf5b06af8a78257af42b8a8c8e4d448ea663
ktjd1262 <k9326239@gmail.com>
2018-01-09 02:37:35 +0900

    deply

--- frontend/src/containers/index.async.js
+++ frontend/src/containers/index.async.js
@@ -1,7 +1,7 @@
 import asyncRoute from 'lib/asyncRoute'
 
 export const Main = asyncRoute(() => import('./Main'))
-export const Login = asyncRoute(() => import('/Login'))
+export const Login = asyncRoute(() => import('./Login'))
 export const Register = asyncRoute(() => import('./Register'))
 export const Write = asyncRoute(() => import('./Write'))
 export const Detail = asyncRoute(() => import('./Detail'))
